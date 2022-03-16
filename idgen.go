44df75aba86905c4fea0c5c7b60dc8486d91f8a7
Veauville Raphael <veauville.raphael@gmail.com>
2022-03-16 16:36:24 +0100

    Update

--- idgen.go
+++ idgen.go
@@ -1,13 +1,47 @@
 package idgen
 
-import "time"
+import (
+	"time"
+)
 
-const epoch = 1420070400000 // Jan 1st, 2022
+const epoch = 1640991600000 // Jan 1st, 2022
+const workerID = uint64(0)
+const processID = uint64(1)
+var increment = uint64(0)
 
-func generate() uint64 {
-	timestamp := uint64(time.Now().Unix()) - epoch
-	workerID := uint64(0)
-	processID := uint64(1)
-	increment := uint64(1)
+type Snowflake struct {
+	timestamp uint64
+	workerID  uint64
+	processID uint64
+	increment uint64
+}
+
+func Generate() uint64 {
+	timestamp := uint64(time.Now().UnixNano() / int64(time.Millisecond) - epoch)
+	if increment >= 4096 {
+		increment = 0
+	}
+	increment++
 	return ((timestamp << 22) | (workerID << 17) | (processID << 12) | increment)
+}
+
+func Destructure(id uint64) Snowflake {
+	timestamp := (id >> 22) + epoch
+	workerID := (id >> 17)
+	processID := (id >> 12)
+	increment := id
+	return Snowflake{timestamp, workerID, processID, increment}
+}
+
+func GetTimestamp(id uint64) uint64 {
+	return (id >> 22) + epoch
+}
+func getWorkerID(id uint64) uint64 {
+	return (id >> 17)
+}
+func getProcessID(id uint64) uint64 {
+	return (id >> 12)
+}
+func getIncrement(id uint64) uint64 {
+	return id
 }
--- idgen_test.go
+++ idgen_test.go
@@ -3,12 +3,41 @@ package idgen
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
-func TestNew(t *testing.T) {
-	id := generate()
-	fmt.Println(id)
+func TestGenerate(t *testing.T) {
+	id := Generate()
 	if id == 0 {
-		t.Error("idgen.generate() returned 0")
+		t.Error("Generate() returned 0")
 	}
+}
+func TestDestructure(t *testing.T){
+	id := Generate()
+	snowflake := Destructure(id)
+	if snowflake.timestamp != GetTimestamp(id) {
+		t.Errorf("GetTimestamp() returned %v, expected %v", GetTimestamp(id), snowflake.timestamp)
+	}
+	if snowflake.workerID != getWorkerID(id) {
+		t.Errorf("getWorkerID() returned %v, expected %v", getWorkerID(id), snowflake.workerID)
+	}
+	if snowflake.processID != getProcessID(id) {
+		t.Errorf("getProcessID() returned %v, expected %v", getProcessID(id), snowflake.processID)
+	}
+	if snowflake.increment != getIncrement(id) {
+		t.Errorf("getIncrement() returned %v, expected %v", getIncrement(id), snowflake.increment)
+	}
+}
+
+func TestPerformances(t *testing.T){
+	// Musure time for Generate()
+	start := time.Now()
+	for i := 0; i < 1000000; i++ {
+		Generate()
+	}
+	elapsed := time.Since(start)
+	if elapsed.Nanoseconds() / 1000000 > 7 {
+		t.Errorf("Generate() took %v", elapsed)
+	}
+	fmt.Println("\n- Generate() took", elapsed, "\n- Nanoseconds per id:", elapsed.Nanoseconds() / 1000000, "\n\u200b")
 }
