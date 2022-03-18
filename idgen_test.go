e0ed8dfec3b888ed75bf28c83991fd530bdedd60
Veauville Raphael <veauville.raphael@gmail.com>
2022-03-18 10:21:06 +0100

    Fix tests

--- idgen_test.go
+++ idgen_test.go
@@ -19,13 +19,13 @@ func TestDestructure(t *testing.T){
 		t.Errorf("GetTimestamp() returned %v, expected %v", GetTimestamp(id), snowflake.timestamp)
 	}
 	if snowflake.workerID != GetWorkerID(id) {
-		t.Errorf("getWorkerID() returned %v, expected %v", getWorkerID(id), snowflake.workerID)
+		t.Errorf("GetWorkerID() returned %v, expected %v", GetWorkerID(id), snowflake.workerID)
 	}
 	if snowflake.processID != GetProcessID(id) {
-		t.Errorf("getProcessID() returned %v, expected %v", getProcessID(id), snowflake.processID)
+		t.Errorf("GetProcessID() returned %v, expected %v", GetProcessID(id), snowflake.processID)
 	}
 	if snowflake.increment != GetIncrement(id) {
-		t.Errorf("getIncrement() returned %v, expected %v", getIncrement(id), snowflake.increment)
+		t.Errorf("GetIncrement() returned %v, expected %v", GetIncrement(id), snowflake.increment)
 	}
 }
 
